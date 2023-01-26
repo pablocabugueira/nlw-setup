7e1f07609053cc470601c83af4a38bcd5da231a8
Pablo Cabugueira <pablo.cabugueira1234@gmail.com>
2023-01-26 17:52:07 -0300

    save local storage

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const nlwSetup = new NLWSetup(form)
 const button = document.querySelector("header button")
 
 button.addEventListener("click", add)
+form.addEventListener("change", save)
 
 function add() {
     const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
@@ -15,6 +16,12 @@ function add() {
 
     nlwSetup.addDay(today)
 }
+
+function save() {
+    localStorage.setItem("NLWSetup@Habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@Habits")) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
